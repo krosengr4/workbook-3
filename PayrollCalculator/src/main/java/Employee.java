98d1aadc862817b015abe90622aeb83d9297f40d
krosengr4 <rosenkev4@gmail.com>
2025-04-22 14:13:34 -0500

    Created constructor with getters and setters for instances of employee

--- PayrollCalculator/src/main/java/Employee.java
+++ PayrollCalculator/src/main/java/Employee.java
@@ -1,4 +1,52 @@
 public class Employee {
+    // declaring variables
+    int employeeId;
+    String name;
+    double hoursWorked;
+    double payRate;
+
+    //Constructor to create new employee instance
+    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    //region Getters and Setters
+    public int getEmployeeId() {
+        return employeeId;
+    }
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+    //endregion
+
+    //Method to calculate an employees gross pay
+    public double calculateGrossPay() {
+        return this.hoursWorked * this.payRate;
+    }
 
 
 
--- PayrollCalculator/src/main/java/Main.java
+++ PayrollCalculator/src/main/java/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Employee p1 = new Employee(10, "Kevin", 8, 110);
+
+        double grossPay;
+
+        grossPay = p1.calculateGrossPay();
+
+        System.out.println(grossPay);
+    }
+
 }
