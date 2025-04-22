5e711feea68406ceac36b32817740beb0b8c1762
krosengr4 <rosenkev4@gmail.com>
2025-04-22 13:23:30 -0500

    fixed the path so that it would work on anyone's device instead of just my path

--- BedtimeStories/src/main/java/BedtimeStories.java
+++ BedtimeStories/src/main/java/BedtimeStories.java
@@ -1,4 +1,5 @@
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class BedtimeStories {
@@ -7,10 +8,12 @@ public class BedtimeStories {
 
         Scanner userPrompt = new Scanner(System.in);
 
+        // ask user to choose one of the three stories
         System.out.println("1: Goldilocks and the Three Bears \n 2: Hansel and Gretel \n 3: Mary had a Little Lamb.");
         System.out.println("\n Choose a number 1-3 to choose a bedtime story to read: ");
         int userInput = userPrompt.nextInt();
 
+        // switch case to handle user input
         switch (userInput) {
             case 1:
                 readGoldilocks();
@@ -26,74 +29,64 @@ public class BedtimeStories {
         }
     }
 
+    // Method to print out goldilocks story
     static void readGoldilocks() {
-        FileInputStream fis = null;
-        try{
-            fis = new FileInputStream("/Users/rosen/pluralsight/workbook-3/BedtimeStories/src/main/resources/DataFiles/goldilocks.txt");
 
-            Scanner readFile = new Scanner(fis);
+        InputStream is = BedtimeStories.class.getResourceAsStream("/DataFiles/goldilocks.txt");
+
+        try (Scanner readStory = new Scanner(is)) {
 
             String input;
-            int i =1;
+            int i = 1;
 
-            while (readFile.hasNextLine()) {
-                input = readFile.nextLine();
+            while (readStory.hasNextLine()) {
+                input = readStory.nextLine();
                 System.out.println(i + "." + input);
                 i++;
             }
-            readFile.close();
         } catch (Exception e) {
             System.out.println("ERROR! File was not found!");
         }
 
     }
 
+    // Method to print out hansel and gretel story
     static void readHanselAndGretel() {
-        FileInputStream fis = null;
 
-        try {
-            fis = new FileInputStream("/Users/rosen/pluralsight/workbook-3/BedtimeStories/src/main/resources/DataFiles/hansel_and_gretel.txt");
+        InputStream is = BedtimeStories.class.getResourceAsStream("/DataFiles/hansel_and_gretel.txt");
 
-            Scanner readFile = new Scanner(fis);
+        try (Scanner readStory = new Scanner(is)) {
 
             String input;
             int i = 1;
 
-            while (readFile.hasNextLine()) {
-                input = readFile.nextLine();
+            while (readStory.hasNextLine()) {
+                input = readStory.nextLine();
                 System.out.println(i + "." + input);
                 i++;
             }
-
-            readFile.close();
-
         } catch (Exception e) {
             System.out.println("ERROR! File was not found!");
         }
     }
 
+    // Method to print out mary had a little lamb story
     static void readMaryHadLittleLamb() {
-        FileInputStream fis = null;
 
-        try {
-            fis = new FileInputStream("/Users/rosen/pluralsight/workbook-3/BedtimeStories/src/main/resources/DataFiles/mary_had_a_little_lamb.txt");
+        InputStream is = BedtimeStories.class.getResourceAsStream("/DataFiles/mary_had_a_little_lamb.txt");
 
-            Scanner readFile = new Scanner(fis);
+        try (Scanner readStory = new Scanner(is)) {
 
             String input;
             int i = 1;
 
-            while (readFile.hasNextLine()) {
-                input = readFile.nextLine();
+            while (readStory.hasNextLine()) {
+                input = readStory.nextLine();
                 System.out.println(i + "." + input);
                 i++;
             }
-
-            readFile.close();
-
         } catch (Exception e) {
             System.out.println("ERROR! File was not found!");
         }
     }
-
 }
--- ClassNotes/src/main/java/com/pluralsight/fileScanner.java
+++ ClassNotes/src/main/java/com/pluralsight/fileScanner.java
@@ -1,24 +1,42 @@
-package com.pluralsight.fileScanner;
+package com.pluralsight;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.InputStream;
 
 public class fileScanner {
 
     public static void main(String[] args) {
 
-        fileIo();
+//        fileIo();
 
+        fileScanner();
     }
 
     //! The com.pluralsight.Scanner class can be used to read a file.
-    static void fileIo() {
+    static void fileScanner() {
+        InputStream is = fileScanner.class.getResourceAsStream("/poem.txt");
 
+//        if (is == null) {
+//            System.out.println("ERROR! File not Found!");
+//            return;
+//        }
+        try (Scanner myScanner = new Scanner(is)) {
+            while (myScanner.hasNextLine()) {
+                System.out.println(myScanner.nextLine());
+            }
+            } catch(Exception e){
+                System.out.println("ERROR! File not found");
+            }
+
+    }
+
+    // Another way to do this
+    static void fileIo() {
         FileInputStream fis = null;
         try {
             //Create FileInput Stream that points to a specific file.
-            fis = new FileInputStream("/Users/rosen/pluralsight/workbook-3/ClassNotes/src/main/java/com/pluralsight/fileScanner/poem.txt");
+            fis = new FileInputStream("/Users/rosen/pluralsight/workbook-3/ClassNotes/src/main/resources/poem.txt");
 
             //Create an instance of scanner to read the file above
             Scanner myScanner = new Scanner(fis);
