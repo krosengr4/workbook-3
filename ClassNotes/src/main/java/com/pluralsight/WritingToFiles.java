1be67caffeeb7aa0a239077dc22027d6440856a2
krosengr4 <rosenkev4@gmail.com>
2025-04-23 12:00:20 -0500

    Added notes on how to use buffered writer to write to files

--- ClassNotes/src/main/java/com/pluralsight/WritingToFiles.java
+++ ClassNotes/src/main/java/com/pluralsight/WritingToFiles.java
@@ -1,13 +1,20 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WritingToFiles {
 
     public static void main(String[] args) {
+        //calling methods
+        fileWriter();
+        bufferedFileWriter();
 
-        try{
+    }
+
+    static public void fileWriter() {
+        try {
             //open the file writer
             FileWriter writer = new FileWriter("\\Users\\rosen\\pluralsight\\workbook-3\\ClassNotes\\src\\main\\resources\\skills.txt");
 //            FileWriter writer = new FileWriter("skills.txt");
@@ -22,11 +29,34 @@ public class WritingToFiles {
             //close the writer when done
             writer.close();
 
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
             System.err.println("ERROR");
         }
-
     }
 
+    static public void bufferedFileWriter() {
+
+        try {
+            //create a file writer
+            FileWriter fileWriter = new FileWriter("C:\\Users\\rosen\\pluralsight\\workbook-3\\ClassNotes\\src\\main\\resources\\text.txt");
+
+            //Create a buffered writer
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+
+            //write to the file
+            String text;
+            for (int i = 0; i <= 10; i++) {
+                text = String.format("Counting %d\n", i);
+                bufWriter.write(text);
+            }
+
+            //close the writer
+            bufWriter.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.err.println("ERROR");
+        }
+    }
 }
