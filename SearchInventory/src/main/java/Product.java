d1ee72bd6f475b69aaf5613c96520c778ea36ee1
krosengr4 <rosenkev4@gmail.com>
2025-04-23 13:56:33 -0500

    added a constructor and getters for product object

--- SearchInventory/src/main/java/Product.java
+++ SearchInventory/src/main/java/Product.java
@@ -1,2 +1,30 @@
 public class Product {
+
+    //Declare variables
+    private int id;
+    private String name;
+    private float price;
+
+    //region Getters
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    //endregion
+
+
+    public Product(int id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+
+
 }
