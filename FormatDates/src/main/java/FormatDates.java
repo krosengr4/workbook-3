8ae9dbc1553b1917c198e627c3ddb8f4bd5f083c
krosengr4 <rosenkev4@gmail.com>
2025-04-23 11:07:56 -0500

    Completed format dates exercise. WB 3 Mod 2 Exercise 2

--- FormatDates/src/main/java/FormatDates.java
+++ FormatDates/src/main/java/FormatDates.java
@@ -1,7 +1,30 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDates {
 
     public static void main(String[] args) {
 
+        //Creating variables for the date + date and time right now
+        LocalDate todayDate = LocalDate.now();
+        LocalDateTime todayDateTime = LocalDateTime.now();
+
+        //Setting the formats that we went for date and time
+        DateTimeFormatter mdy = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        DateTimeFormatter monthName = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+        DateTimeFormatter gmt = DateTimeFormatter.ofPattern("EEEE, MMM d, yyyy HH:mm");
+
+        //Formatting the date and time
+        String mdyFormat = todayDate.format(mdy);
+        String fullMonthName = todayDate.format(monthName);
+        String gmtTime = todayDateTime.format(gmt);
+
+        //Print out results
+        System.out.println("Today's date: " + mdyFormat);
+        System.out.println("Today's date: " + todayDate);
+        System.out.println("Today's date: " + fullMonthName);
+        System.out.println("Date and Time: " + gmtTime);
     }
 
 }
