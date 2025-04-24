0c51c00d7bb95fd614ca83d3b49bd397c60aecbd
krosengr4 <rosenkev4@gmail.com>
2025-04-24 10:09:10 -0500

    Added more notes for HashMaps on how to iterate through one

--- ClassNotes/src/main/java/com/pluralsight/HashMapNotes.java
+++ ClassNotes/src/main/java/com/pluralsight/HashMapNotes.java
@@ -12,8 +12,10 @@ public class HashMapNotes {
         //Calling methods
         addingAndRemoving();
         lookingUpItems();
+        iterateThrough();
     }
 
+    //! How to add and remove key/value pairs
     public static void addingAndRemoving() {
         //Creating a HashMap called statesAndCapitals where both key and value are Strings.
         HashMap<String, String> statesAndCapitals = new HashMap<String, String>();
@@ -30,11 +32,12 @@ public class HashMapNotes {
         statesAndCapitals.remove("CA");
         System.out.println("HashMap: " + statesAndCapitals);
 
-        //You can also remove all items by using the .clear() method
+        //! Remove all items by using the .clear() method
         statesAndCapitals.clear();
         System.out.println("HashMap: " + statesAndCapitals);
     }
 
+    //! How to look up items
     public static void lookingUpItems() {
         HashMap<String, String> statesAndCapitals = new HashMap<String, String>();
 
@@ -46,5 +49,22 @@ public class HashMapNotes {
 
         //Use the .get() method and the key to access, and it returns the value
         System.out.println("The capital of Texas is: " + statesAndCapitals.get("TX")); //<--- Output: "The capital of Texas is: Austin"
+    }
+
+    //! Iterating through a hashmap
+    public static void iterateThrough() {
+        HashMap<String, String> statesAndCapitals = new HashMap<String, String>();
+
+        //use .put() method to add to a hashmap
+        statesAndCapitals.put("TX", "Austin");
+        statesAndCapitals.put("FL", "Tallahassee");
+        statesAndCapitals.put("CA", "Sacramento");
+        statesAndCapitals.put("AL", "Montgomery");
+
+        //To get only the values
+        for(String value : statesAndCapitals.values()) {
+            System.out.println(value);
+        }
+
     }
 }
