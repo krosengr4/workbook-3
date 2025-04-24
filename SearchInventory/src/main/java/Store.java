e4509053e1edfc7ab1ac5bce6b48c3e51adc4f0f
krosengr4 <rosenkev4@gmail.com>
2025-04-24 11:36:04 -0500

    Completed the bonus that reads the products from csv file

--- SearchInventory/src/main/java/Store.java
+++ SearchInventory/src/main/java/Store.java
@@ -6,8 +6,10 @@ import java.util.Scanner;
 public class Store {
 
     public static void main(String[] args) {
+        //Create file path for inventory csv
+        String filePath = "SearchInventory/src/main/resources/inventory.csv";
 
-        ArrayList<Product> inventory = getInventory();
+        ArrayList<Product> inventory = getInventory(filePath);
 
         Scanner myScanner = new Scanner(System.in);
 
@@ -16,17 +18,18 @@ public class Store {
         //For loop that prints out every product's attributes
         for (int i = 0; i < inventory.size(); i++) {
             Product p = inventory.get(i);
-            System.out.printf("id: %d %s - Price: $%.2f", p.getId(), p.getName(), p.getPrice());
+            System.out.println("id: " + p.getId() + " Name: " + p.getName() + " Price: " + p.getPrice());
         }
     }
 
-    private static ArrayList<Product> getInventory(){
-        FileReader reader;
+    private static ArrayList<Product> getInventory(String filePath){
+        // Create array list for inventory
+        ArrayList<Product> inventory = new ArrayList<Product>();
 
         try{
-            reader = new FileReader("SearchInventory/src/main/resources/inventory.csv");
-            String input;
+            FileReader reader = new FileReader(filePath);
             BufferedReader bufReader = new BufferedReader(reader);
+            String input;
 
             while((input = bufReader.readLine()) != null) {
                 String[] lineData = input.split("\\|");
@@ -35,26 +38,13 @@ public class Store {
                 String productName = lineData[1];
                 double productPrice = Double.parseDouble(lineData[2]);
 
-
-                ArrayList<Product> inventory = new ArrayList<Product>();
-                for (int i = 0; i < lineData.length; i++) {
-                    Product newProduct = new Product(productId, productName, productPrice);
-                    inventory.add(newProduct);
-
-                }
-
-                return inventory;
+                Product newProduct = new Product(productId, productName, productPrice);
+                inventory.add(newProduct);
             }
-
-
-
 
         } catch (Exception e) {
             System.err.println("ERROR: " + e.getMessage());
         }
-
-        ArrayList<Product> inventory = new ArrayList<Product>();
-
         return inventory;
     }
 
