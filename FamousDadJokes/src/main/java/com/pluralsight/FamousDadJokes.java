3ab74a851ba0361385abfb928ee7cc4af6ffe98e
krosengr4 <rosenkev4@gmail.com>
2025-04-24 00:02:01 -0500

    improved messages to user, added comments

--- FamousDadJokes/src/main/java/com/pluralsight/FamousDadJokes.java
+++ FamousDadJokes/src/main/java/com/pluralsight/FamousDadJokes.java
@@ -6,6 +6,7 @@ public class FamousDadJokes {
 
     public static void main(String[] args) {
 
+        //Create an array with dad jokes
         String[] dadJokes = {
                 "What do you call a magician who lost his magic? \n Ian. ",
                 "How do computers get drunk? \n They take screenshots. ",
@@ -19,15 +20,20 @@ public class FamousDadJokes {
                 "Why don't skeletons fight each other? \n They don't have the guts."
         };
 
+        //Open scanner
         Scanner myScanner = new Scanner(System.in);
 
         try {
+            //User choice determine wether to stay in loop or exit app
             String userChoice = "y";
 
+            //While loop to prompt user to hear more dad jokes
             while (userChoice.equalsIgnoreCase("y")) {
+                //Get user input
                 System.out.println("Choose a number from 1-10 for a dad joke: ");
                 int jokeNumber = myScanner.nextInt();
 
+                //Switch case to determine which joke to print out
                 switch (jokeNumber) {
                     case 1:
                         System.out.println(dadJokes[0]);
@@ -63,12 +69,13 @@ public class FamousDadJokes {
                         System.out.println("ERROR! Must print a number between 1-10");
                 }
 
+                //Get user input if they want to hear another joke
                 System.out.println("\n Would you like to hear another joke? (enter y or n): ");
                 userChoice = myScanner.next();
-                userChoice = userChoice.toLowerCase();
 
+                //If user selects n, exit the app
                 if (userChoice.equalsIgnoreCase("n")) {
-                    System.out.println("Thank you!");
+                    System.out.println("Exiting Famous Dad Jokes application. \nThank you!");
                     break;
                 }
             }
