03a6ea458f6272f080be590ad1641e9b0f85a35b
krosengr4 <rosenkev4@gmail.com>
2025-04-24 12:24:10 -0500

    Completed search inventory map exercise with comments. WB 3 Mod 4 Exercise 1

--- SearchInventory/src/main/java/Store.java
+++ SearchInventory/src/main/java/Store.java
@@ -47,27 +47,4 @@ public class Store {
         }
         return inventory;
     }
-
-//    public static ArrayList<Product> getInventory() {
-//
-//        //Create an array list that contains the Product object
-//        ArrayList<Product> inventory = new ArrayList<Product>();
-//
-//        //Create new products to put into inventory
-//        Product beans = new Product(1, "Busch's Baked Beans", 7.99);
-//        Product soup = new Product(2, "Campbell's Chicken and Noodle Soup", 3.98);
-//        Product pretzels = new Product(3, "Snyder's of Hanover Pretzel Sticks", 4.29);
-//        Product milk = new Product(4, "Stonyfield Farm Whole Milk 1/2 gal", 5.29);
-//        Product sourCream = new Product(5, "Daisy Sour Cream", 3.47);
-//
-//        //Put the items we created into inventory array list
-//        inventory.add(beans);
-//        inventory.add(soup);
-//        inventory.add(pretzels);
-//        inventory.add(milk);
-//        inventory.add(sourCream);
-//
-//        return inventory;
-//    }
-
 }
--- SearchInventoryMap/src/main/java/SearchInventoryMap.java
+++ SearchInventoryMap/src/main/java/SearchInventoryMap.java
@@ -1,48 +1,63 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class SearchInventoryMap {
 
     public static void main(String[] args) {
-
+        //Add file path for inventory csv
         String filePath = "SearchInventoryMap/src/main/resources/inventory.csv";
 
+        //have inventory HashMap call loadInventory method
         HashMap<String, Product> inventory = loadInventory(filePath);
 
+        //Get user input
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter a product you would like to search: ");
+        String userInput = myScanner.nextLine();
+
+        //Match the user input to the product name. Print product if it is in inventory HashMap
+        Product matchProduct = inventory.get(userInput);
+        if (matchProduct == null) {
+            System.out.println("We don't have that product in stock");
+        } else {
+            System.out.println("Here is what we have in stock:\n" + matchProduct.getName() + " $" + matchProduct.getPrice());
+        }
     }
 
-    private static HashMap<String, Product> loadInventory(String filePath){
+    //Method to read inventory.csv and add to inventory HashMap
+    private static HashMap<String, Product> loadInventory(String filePath) {
 
+        //Create new HashMap for inventory
         HashMap<String, Product> inventory = new HashMap<String, Product>();
 
-        try{
+        try {
+            //Open FileReader and BufferedFileReader
             FileReader reader = new FileReader(filePath);
             BufferedReader bufReader = new BufferedReader(reader);
 
             String input;
-            while((input = bufReader.readLine()) != null) {
+
+            //While loop to read file and create new product for each line
+            while ((input = bufReader.readLine()) != null) {
+                //Split file based at "|"
                 String[] lineData = input.split("\\|");
 
-                System.out.println(lineData[0]);
-
+                //Take the different parts of file, put them in variables
                 int productId = Integer.parseInt(lineData[0]);
                 String productName = lineData[1];
                 double productPrice = Double.parseDouble(lineData[2]);
 
+                //Create new product and put new product in inventory HashMap
                 Product newProduct = new Product(productId, productName, productPrice);
-
-                inventory.put(productName, newProduct);
+                inventory.put(newProduct.getName(), newProduct);
             }
 
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        System.out.println(inventory.get("Hammer"));
+        //Return inventory HashMap
         return inventory;
-
     }
-
-
 }
